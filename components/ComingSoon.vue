add9f2de1f47c0e7708e5d2fae9c7343fdadbf87
Joel Dorne <joel@objective.agency>
2024-03-02 12:07:32 +0800

    Added Coming Soon Page

---
+++ components/ComingSoon.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div
+      class="min-h-screen bg-gray-900 flex flex-col items-center justify-center p-4"
+    >
+      <h1 class="text-5xl text-white font-bold mb-8 animate-pulse text-center">
+        Coming Soon
+      </h1>
+      <p class="text-white text-lg mb-8 text-center">
+        We're working hard to bring you something amazing. Stay tuned!
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
